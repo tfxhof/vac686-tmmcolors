cbfa6e07cc34148bc4ffd71fabd8921c9a18fd0a
PCVICTOR\victo <victorargueso79@gmail.com>
2022-11-16 17:04:17 +0100

    Commit inicial

---
+++ api_color/src/calculo_color.py
@@ -0,0 +1,11 @@
+import colorpy.illuminants
+import colorpy.colormodels
+from tmm import color
+
+
+def calcula_rgb(n_fn_list, d_list, th_0):
+    reflectances = color.calc_reflectances(n_fn_list, d_list, th_0)
+    illuminant = colorpy.illuminants.get_illuminant_D65()
+    spectrum = color.calc_spectrum(reflectances, illuminant)
+    color_dict = color.calc_color(spectrum)
+    return color_dict['rgb']
---
+++ api_color/src/pruebas.py
@@ -0,0 +1,12 @@
+import unittest
+import calculo_color
+
+
+class TestMyModule(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(calculo_color.calcula_rgb(n_fn_list, d_list, th_0)
+
+
+if __name__ == "__main__":
+    unittest.main()
