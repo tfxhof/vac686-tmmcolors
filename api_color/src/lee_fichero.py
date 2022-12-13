ea74fdfd0031ac12a2ee2dc30c1a6b902bc85096
PCVICTOR\victo <victorargueso79@gmail.com>
2022-12-13 15:59:35 +0100

    leer

--- api_color/src/lee_fichero.py
+++ api_color/src/lee_fichero.py
@@ -5,10 +5,20 @@ import colorpy.colormodels
 import pandas as pd
 from spyder_kernels.utils.lazymodules import pandas
 from tmm import color
+from numpy import pi, linspace, inf, array
 
-def leer_fichero():
+def leer_fichero_si():
 
-    df = pd.read_csv("prueba.txt", delimeter= '\t')
+    df = pd.read_csv("/documets/m_Au.txt", delimeter= '\t')
 
-    resul= round(df,3)
+    df = array(df)
+    matriz = pd.DataFrame(df, columns=['A', 'B'])
+    column_b = matriz['B']
+
+    def leer_fichero_au():
+        df = pd.read_csv("/documets/m_SI.txt", delimeter='\t')
+
+        df = array(df)
+        matriz = pd.DataFrame(df, columns=['A', 'B'])
+        column_b = matriz['B']
 
