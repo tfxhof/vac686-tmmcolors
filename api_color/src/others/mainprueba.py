5ab4b9ba9698c70bdbc5c75fb2c366ba9e30e5b5
PCVICTOR\victo <victorargueso79@gmail.com>
2024-06-09 15:28:17 +0200

    cambios

--- api_color/src/others/mainprueba.py
+++ api_color/src/others/mainprueba.py
@@ -1,4 +1,11 @@
+import json
+import os
+from importlib.resources import Resource
+
+from flask import jsonify, send_file
+
 from src.calculo_color.lee_fichero import leer_fichero
+from src.rest.api import api
 
 
 def saludar(nombre):
@@ -23,3 +30,58 @@ def main():
 if __name__ == "__main__":
     # Llamar a la función principal
     main()
+
+
+@api.route('/materiales/')
+class obtenerMateriales(Resource):
+    def get(self):
+        ruta_json = os.path.join(os.path.dirname(__file__), '..', '..', 'documents', 'catalogo.json')
+        if os.path.exists(ruta_json):
+            return send_file(ruta_json, mimetype='application/json')
+
+
+        else:
+            # Si el archivo no existe, retorna un mensaje de error
+            return {'error': 'Archivo JSON no encontrado'}, 404
+@api.route('/materiales/<int:objeto_id>')
+class ObtenerMaterialPorID(Resource):
+    def get(self, objeto_id):
+        print(1)
+        ruta_json = os.path.join(os.path.dirname(__file__), '..', '..', 'documents', 'catalogo.json')
+        if os.path.exists(ruta_json):
+            with open(ruta_json, 'r') as json_file:
+                data = json.load(json_file)
+
+                objeto = next((item for item in data if item["id"] == objeto_id), None)
+            if objeto:
+                return objeto
+            else:
+                # Si el objeto no existe, se retorna un mensaje de error con código 404
+                return {'error': 'Objeto no encontrado'}, 404
+
+
+        else:
+            # Si el archivo no existe, retorna un mensaje de error
+            return {'error': 'Archivo JSON no encontrado'}, 404
+
+
+@api.route('/materiales/nombre/<int:objeto_id>/')
+class ObtenerNombredeMaterialPorID(Resource):
+    def get(self, objeto_id):
+        print(1)
+        ruta_json = os.path.join(os.path.dirname(__file__), '..', '..', 'documents', 'catalogo.json')
+        if os.path.exists(ruta_json):
+            with open(ruta_json, 'r') as json_file:
+                data = json.load(json_file)
+
+                objeto = next((item for item in data if item["id"] == objeto_id), None)
+            if objeto:
+                return jsonify(objeto["label"])
+            else:
+                # Si el objeto no existe, se retorna un mensaje de error con código 404
+                return {'error': 'Objeto no encontrado'}, 404
+
+
+        else:
+            # Si el archivo no existe, retorna un mensaje de error
+            return {'error': 'Archivo JSON no encontrado'}, 404
